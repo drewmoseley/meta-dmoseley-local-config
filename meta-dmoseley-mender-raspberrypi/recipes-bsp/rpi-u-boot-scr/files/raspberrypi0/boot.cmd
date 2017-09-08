b4d476b4dd31110591d5a3abcd3d8363578f246d
Drew Moseley <drew.moseley@northern.tech>
2017-09-08 19:10:28 -0400

    rpi: Move fix to meta-mender layer.
    It turns out the temporary fix I had locally was actually
    the right thing to do.  I'm currently submitting the proper
    fix upstream and thus we don't need this here any more.
    
    Signed-off-by: Drew Moseley <drew.moseley@northern.tech>

--- meta-dmoseley-mender-raspberrypi/recipes-bsp/rpi-u-boot-scr/files/raspberrypi0/boot.cmd
+++
@@ -1,7 +0,0 @@
-fdt addr ${fdt_addr} && fdt get value bootargs /chosen bootargs
-run mender_setup
-setenv bootargs earlyprintk console=tty0 console=ttyAMA0,115200 root=${mender_kernel_root} rootfstype=ext4 rootwait noinitrd
-mmc dev ${mender_uboot_dev}
-load ${mender_uboot_root} ${kernel_addr_r} /boot/uImage
-bootm ${kernel_addr_r} - ${fdt_addr}
-run mender_try_to_recover
